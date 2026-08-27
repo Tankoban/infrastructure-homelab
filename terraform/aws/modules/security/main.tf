b539fa78874a33809a13a11f8dc2a1d599aebcdc
Tankoban <tankoban@gmail.com>
2026-08-27 14:16:11 -0400

    fix: correctly suppress Checkov SG attachment false positive

--- terraform/aws/modules/security/main.tf
+++ terraform/aws/modules/security/main.tf
@@ -1,5 +1,7 @@
-#checkov:skip=CKV2_AWS_5:Security group is attached to EC2 through the root module and compute module variable; Checkov does not resolve this cross-module relationship.
+
 resource "aws_security_group" "web" {
+  #checkov:skip=CKV2_AWS_5:Security group is attached to EC2 through the root module and compute module variable; Checkov does not resolve this cross-module relationship
+
   name        = "tf-homelab-web-sg"
   description = "Security group for Terraform homelab web server"
   vpc_id      = var.vpc_id
